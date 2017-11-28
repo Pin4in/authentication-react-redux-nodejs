d3b08d991da0ce997cdcb14850c303c02cc1640b
pin4in <sergii.korkh@gmail.com>
2017-11-29 00:37:31 +0200

    fix path

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import authReducer from './auth_reducer';
+import authReducer from './auth';
 import userReducer from './user';
 
 const rootReducer = combineReducers({
