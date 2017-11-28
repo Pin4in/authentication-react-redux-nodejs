c0a39c7613998103d10c4435b7cf782abaf75d08
pin4in <sergii.korkh@gmail.com>
2017-11-29 00:37:11 +0200

    cleanup RequireAuth

--- client/src/components/RequireAuth/RequireAuth.js
+++ client/src/components/RequireAuth/RequireAuth.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
     render() {
       const { authenticated } = this.props.auth;
-      const { from } = this.props.location.state || { from: { pathname: '/' } }
+      const defaultPath ={ pathname: '/' };
       if ( !authenticated) {
         return (
-          <Redirect to={from}/>
+          <Redirect to={defaultPath}/>
         )
       }      
       return <ComposedComponent {...this.props} />
