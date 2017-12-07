364e85e0468211b89521dad08a957bd1b342c32d
pin4in <sergii.korkh@gmail.com>
2017-12-07 12:52:31 +0200

    user signed in if has token

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,14 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
+import { AUTH_USER } from './actions/types';
+
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // we need to update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render((
   <Provider store={store}>
